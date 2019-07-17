52202296f58fe49195b0c6d89145754d8d48bd23
Marcos Lourinho <marcoslourinho@protonmail.com>
2019-07-17 09:42:25 -0300

    final project

---
+++ models/Cliente.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once "ValidadorCPF.php";
+require_once "ValidadorCNPJ.php";
+
+class Cliente
+{
+
+  var $nome;
+  var $cpf_cnpj;
+  var $telefone;
+  var $email;
+  var $cep;
+  var $endereco;
+  var $bairro;
+  var $numero;
+  var $cidade;
+  var $uf;
+
+  function __construct(
+    $nome,
+    $cpf_cnpj,
+    $telefone,
+    $email,
+    $cep,
+    $endereco,
+    $bairro,
+    $numero,
+    $cidade,
+    $uf
+  ) {
+
+    $validadorCPF = new ValidadorCPF();
+    $validadorCNPJ = new ValidadorCNPJ();
+
+    //validação
+    if (!$this->cepValido($cep)) throw new Exception("CEP no formato inválido");
+
+    if (!$this->telefoneValido($telefone)) throw new Exception("Telefone no formato inválido");
+
+    if (!$this->emailValido($email)) throw new Exception("Email no formato inválido");
+
+    if (strlen($cpf_cnpj) > 14) {
+      if (!$validadorCNPJ->ehValido($cpf_cnpj)) throw new Exception("CNPJ inválido");
+    } else {
+      if (!$validadorCPF->ehValido($cpf_cnpj)) throw new Exception("CPF Inválido");
+    }
+
+    $this->nome = $nome;
+    $this->cpf_cnpj = $this->removeFormatacao($cpf_cnpj);
+    $this->telefone = $this->removeFormatacao($telefone);
+    $this->email = $email;
+    $this->cep = $this->removeFormatacao($cep);
+    $this->endereco = $endereco;
+    $this->bairro = $bairro;
+    $this->numero = $numero;
+    $this->cidade = $cidade;
+    $this->uf = $uf;
+  }
+
+  function cepValido($cep)
+  {
+    if (strlen($cep) == 10) {
+      //22.333-333
+      $regex_cep = "/[0-9]{2}\.[0-9]{3}\-[0-9]{3}/";
+      return preg_match($regex_cep, $cep);
+    } else {
+      return false;
+    }
+  }
+
+  function telefoneValido($telefone)
+  {
+    if (strlen($telefone) == 15) {
+      //(99) 99999-9999
+      $regex_telefone = "/\([0-9]{2}\)[0-9]{5}\-[0-9]{4}/";
+      return preg_match($regex_telefone, str_replace(" ", "", $telefone));
+    } else {
+      return false;
+    }
+  }
+
+  function emailValido($email)
+  {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function removeFormatacao($info)
+  {
+    $dado = str_replace([".", "-", "/", "(", ")", " "], "", $info);
+    return $dado;
+  }
+}
---
+++ models/Viagem.php
@@ -0,0 +1,84 @@
+<?php
+
+class Viagem
+{
+
+  var $origem;
+  var $destino;
+  var $data_ida;
+  var $data_volta;
+  var $classe;
+  var $adultos;
+  var $criancas;
+  var $preco;
+
+  function __construct(
+    $origem,
+    $destino,
+    $data_ida,
+    $data_volta,
+    $classe,
+    $adultos,
+    $criancas,
+    $preco
+  ) {
+
+    if (!$this->dataValida($data_ida)) throw new Exception("Data de Ida Inválida");
+
+    if (!$this->dataValida($data_volta)) throw new Exception("Data de Volta Inválida");
+
+    if (!$this->precoValido($preco)) throw new Exception("Preço Inválido");
+
+    $this->origem = $origem;
+    $this->destino = $destino;
+    $this->data_ida = $data_ida;
+    $this->data_volta = $data_volta;
+    $this->classe = $classe;
+    $this->adultos = $adultos;
+    $this->criancas = $criancas;
+    $this->preco = $this->convertePreco($preco);
+  }
+
+
+  public function dataValida($data)
+  {
+
+    // 10/01/2019
+    // 2019-01-10
+
+    if (strlen($data) != 10) return false;
+
+    if (!strpos($data, "-")) return false;
+
+    $partes = explode("-", $data);
+
+    $ano = $partes[0];
+    $mes = $partes[1];
+    $dia = $partes[2];
+
+    if (strlen($ano) < 4) return false;
+
+    if (!checkdate($mes, $dia, $ano)) return false;
+
+    $data_atual = date("Y-m-d");
+
+    if (strtotime($data) < strtotime($data_atual)) return false;
+
+    return true;
+  }
+
+  public function precoValido($preco)
+  {
+    $regex_preco = "/^[0-9]{1,3}([.][0-9]{3})*[,][0-9]{2}$/";
+    return preg_match($regex_preco, $preco);
+  }
+
+  public function convertePreco($preco)
+  {
+    //1.500,35
+    //1500.35 double
+    $numero_valido = str_replace(",", ".", $preco);
+    $numero_valido = str_replace(".", "", substr($numero_valido, 0, -3)) . substr($numero_valido, -3);
+    return doubleval($numero_valido);
+  }
+}
